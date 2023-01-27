dd1899bafebd681309b208be34cf4983e6041a5b
Kamil Manka <manka.kamil@gmail.com>
2023-01-27 14:38:12 +0100

    Workshop commit

--- src/workshop.jsx
+++ src/workshop.jsx
@@ -1,39 +1,49 @@
 
+Męczennicy ciemności bezbożnictwa rozpędzili, pokazawszy wszystkim
+ludziom światłość poznania Boga.
+
+Naukami prawosławnymi, Bożymi zorzami oświeciliście Kościół Pański,
+mądrzy pasterze.
+
+Skruszyliście sieci biesów, zamieszkawszy w nieprzebytych pustyniach,
+ojcowie czcigodni.
 
 
+Zbawco, nie zawstydź mnie, czyniącego dzieła wstydliwe, gdy przyjdziesz
+sądzić świat cały.
 
+Chwała Tobie, Chryste Boże, Apostołów chwało, męczenników radości,
+którzy głosili Trójcę jednoistotną.
 
+Święci męczennicy, mężnie walczący i ukoronowani, módlcie się do Pana,
+aby zmiłował się nad naszymi duszami.
 
+Poznana zostałaś jako Matka, Bogurodzico, i w
+nadprzyrodzony sposób pozostałaś Dziewicą, co przewyższa słowa i zrozumienie,
+cudu zrodzenia Twego język nie może wypowiedzieć, słowa są bezsilne, Czysta,
+nieosiągalny jest bowiem obraz zrodzenia, gdy bowiem zechce Bóg, zwyciężony
+jest porządek natury, przeto Ciebie wszyscy znając jako Matkę Bożą, prosimy
+Ciebie gorliwie: Módl się o zbawienie dusz naszych.
 
+Także: Pogodna Światłości. Prokimenon dnia.
 
+Nie troszcząc się o wszystko, co ziemskie, święci męczennicy, i na Sądzie
+Chrystusa mężnie głosiliście, odpłatę za męki od Niego przyjęliście, lecz jako
+mający odwagę, módlcie się do Niego jako Boga wszechmocnego, aby zbawił
+dusze nasze, przybiegających do was, błagamy.
 
-Jedyną Ciebie, najświętsza Władczyni, po zrodzeniu poznaliśmy wszyscy
-jako Dziewicę, a Ty bez nasienia zrodzonego przez Ciebie widząc na krzyżu z
-przygwożdżonymi rękoma, płacząc wołałaś, Przeczysta: Dobrowolnie umierasz,
-wielce cierpliwy, wybawiając ze śmierci wszystkich śpiewających Tobie.
-Chwała, i teraz. Stauroteotokion: 
+Chwalebni męczennicy, owieczki duchowe, ofiary całopalne, ofiary
+przyjemne Bogu, was ziemia nie ukryła, lecz niebo przyjęło, staliście się
+współuczestnikami aniołów, z nimi módlcie się, prosimy was, do łaskawcy i Boga
+naszego, aby pokój dał światu i zbawił dusze nasze.
 
-Władczyni najświętsza Dziewico, która zrodziłaś
-Pana z miłosierdzia przygwożdżonego na krzyżu i toczącego światu strumienie
-życia, ubłagaj Go, aby zbawił dusze nasze, Ciebie bowiem jedyną mamy wierni
-jako ucieczkę, mur obronny i wspomożenie, przeto przybiegamy pod opiekę
-Twoją.
+Na obraz Twój i podobieństwo stworzyłeś najpierw człowieka i w
+raju umieściłeś, aby rządził Twoim stworzeniem, ale z zawiści diabła oszukany
+został, skosztował jadu, stał się przestępcą Twoich przykazań, przeto wróciłeś do
+ziemi życie jego, skąd je i wziąłeś, osądziwszy go, Panie, i wyproś pokój.
 
+Daj odpoczynek, Zbawco nasz, Dawco życia, braciom naszym,
+których zabrałeś z czasu, wołających: Panie, chwała Tobie.
 
-
-Także: Pogodna Światłości. Prokimenon dnia, i: Pozwól, Panie, w wieczór ten.
-Na stichownie stichery ku czci krzyża, ton 7. Prosomion:
-Nie jest już nam zabronione drzewo życia, jako nadzieję mającym krzyż
-Twój, Panie, chwała Tobie.
-Stichos: Do Ciebie wznoszę oczy moje, który mieszkasz w niebie. Oto jak oczy sług zwrócone
-są na ręce ich panów i jak oczy służącej na ręce jej pani, tak oczy nasze ku Bogu, Bogu naszemu,
-dopóki nie ulituje się nad nami.
-Powieszony zostałeś na drzewie, Nieśmiertelny, i zniszczyłeś sieci diabła,
-Panie, chwała Tobie.
-Stichos: Zmiłuj się nad nami, Panie, zmiłuj się nad nami, albowiem wzgardą jesteśmy
-nasyceni. Dusza nasza nasycona jest szyderstwem zarozumialców i wzgardą pyszałków.
-Męczennikom: Chwała Tobie, Chryste Boże, Apostołów chwało, męczenników
-radości , którzy głosili Trójcę jednoistotną.
-Chwała, i teraz. Stauroteotokion: Gdy Nieskalana ujrzała Ciebie na krzyżu
-dobrowolnie przygwożdżonego, płacząc opiewała władzę Twoją.
-Także: Teraz pozwalasz odejść. Trisagion. Po: Ojcze nasz, tropariony, ektenia i rozesłanie. 
+Ty, która jedyna Nieogarnionego przyjęłaś i zrodziłaś
+wcielonego Boga Słowo, módl się o zbawienie dusz naszych. 
