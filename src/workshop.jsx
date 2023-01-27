3fe41a895d2c0173ec0e314ab0a1c7713a46cdab
Kamil Manka <manka.kamil@gmail.com>
2023-01-27 21:06:32 +0100

    Revert "Delete .env"
    This reverts commit 38d4dc94fb92f7b50df381d96165b5cf6800d86c.

--- src/workshop.jsx
+++ src/workshop.jsx
@@ -1,49 +1,24 @@
+Wstąpiłeś na krzyż, Jezu, który zstąpiłeś z niebios. Przyszedłeś bowiem na
+śmierć, śycie nieśmiertelne, do będących w ciemnościach Światłość prawdziwa, do
+upadłych zmartwychwstanie wszystkich, nasze oświecenie i Zbawco nasz, chwała
+Tobie.
 
-Męczennicy ciemności bezbożnictwa rozpędzili, pokazawszy wszystkim
-ludziom światłość poznania Boga.
+Sławimy Chrystusa powstałego z martwych, duszę bowiem i ciało
+przyjąwszy, cierpienia od niej odciął, gdy bowiem przeczystą duszą swoją zstąpił
+do otchłani, wtedy zniewolił otchłań. W grobie zaś zniszczenia nie doznało święte 
+ciało Zbawiciela dusz naszych.
 
-Naukami prawosławnymi, Bożymi zorzami oświeciliście Kościół Pański,
-mądrzy pasterze.
+W psalmach i pieśniach sławimy, Chryste, Twoje powstanie z martwych,
+przez które uwolniłeś nas od cierpień otchłani i jako Bóg zesłałeś życie wieczne i
+wielkie miłosierdzie.
 
-Skruszyliście sieci biesów, zamieszkawszy w nieprzebytych pustyniach,
-ojcowie czcigodni.
+O niedościgniony Władco wszystkich, Stwórco niebios i ziemi, który
+cierpiąc na krzyżu mnie wytoczyłeś niecierpiętliwość. Pogrzeb zaś przyjąwszy i
+zmartwychwstawszy w chwale, wskrzesiłeś Adama wszechmocną ręką. Chwała
+Twemu zmartwychwstaniu trzeciego dnia, przez które dałeś nam życie wieczne i
+oczyszczenie grzechów jako jedyny Łaskawy.
 
-
-Zbawco, nie zawstydź mnie, czyniącego dzieła wstydliwe, gdy przyjdziesz
-sądzić świat cały.
-
-Chwała Tobie, Chryste Boże, Apostołów chwało, męczenników radości,
-którzy głosili Trójcę jednoistotną.
-
-Święci męczennicy, mężnie walczący i ukoronowani, módlcie się do Pana,
-aby zmiłował się nad naszymi duszami.
-
-Poznana zostałaś jako Matka, Bogurodzico, i w
-nadprzyrodzony sposób pozostałaś Dziewicą, co przewyższa słowa i zrozumienie,
-cudu zrodzenia Twego język nie może wypowiedzieć, słowa są bezsilne, Czysta,
-nieosiągalny jest bowiem obraz zrodzenia, gdy bowiem zechce Bóg, zwyciężony
-jest porządek natury, przeto Ciebie wszyscy znając jako Matkę Bożą, prosimy
-Ciebie gorliwie: Módl się o zbawienie dusz naszych.
-
-Także: Pogodna Światłości. Prokimenon dnia.
-
-Nie troszcząc się o wszystko, co ziemskie, święci męczennicy, i na Sądzie
-Chrystusa mężnie głosiliście, odpłatę za męki od Niego przyjęliście, lecz jako
-mający odwagę, módlcie się do Niego jako Boga wszechmocnego, aby zbawił
-dusze nasze, przybiegających do was, błagamy.
-
-Chwalebni męczennicy, owieczki duchowe, ofiary całopalne, ofiary
-przyjemne Bogu, was ziemia nie ukryła, lecz niebo przyjęło, staliście się
-współuczestnikami aniołów, z nimi módlcie się, prosimy was, do łaskawcy i Boga
-naszego, aby pokój dał światu i zbawił dusze nasze.
-
-Na obraz Twój i podobieństwo stworzyłeś najpierw człowieka i w
-raju umieściłeś, aby rządził Twoim stworzeniem, ale z zawiści diabła oszukany
-został, skosztował jadu, stał się przestępcą Twoich przykazań, przeto wróciłeś do
-ziemi życie jego, skąd je i wziąłeś, osądziwszy go, Panie, i wyproś pokój.
-
-Daj odpoczynek, Zbawco nasz, Dawco życia, braciom naszym,
-których zabrałeś z czasu, wołających: Panie, chwała Tobie.
-
-Ty, która jedyna Nieogarnionego przyjęłaś i zrodziłaś
-wcielonego Boga Słowo, módl się o zbawienie dusz naszych. 
+Oblubienico dziewicza, któraś Boga w
+niewypowiedziany sposób poczęła w ciele, Matko Boga Najwyższego, przyjmij
+modlitwy sług Twoich, Najczystsza, wszystkim dając oczyszczenie grzechów.
+Teraz przyjmując nasze modlitwy módl się o zbawienie nas wszystkich.
